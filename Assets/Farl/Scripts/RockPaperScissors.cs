d24d18b11d81c99cecf527cc21f2fa84bd2d411b
Farl Lee <spring_snow@hotmail.com>
2019-12-14 16:54:00 +0800

    [Farl] Update changes long time ago

---
+++ Assets/Farl/Scripts/RockPaperScissors.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockPaperScissors : MonoBehaviour {
+
+	public enum State
+	{
+		StartMenu,
+		PlayerTurn,
+		ComTurn,
+		Result
+	}
+
+	State currState;
+	float stateTimer;
+
+	void SetNextState(State nextState)
+	{
+		if (CanEnterState (nextState)) {
+			LeaveState (nextState);
+
+		}
+	}
+
+	void EnterState(State nextState)
+	{
+	}
+
+	void LeaveState(State nextState)
+	{
+	}
+
+	bool CanEnterState(State nextState)
+	{
+	}
+
+	void Update()
+	{
+		UpdateState();
+	}
+
+	void UpdateState()
+	{
+		stateTimer = 
+	}
+}
